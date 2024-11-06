0579fc38ce174b368a970e29067659c1a8c27c52
Tokiya Fukuda <2211140037x@ed.fuk.kindai.ac.jp>
2024-11-06 08:19:43 +0000

    initial commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  config.hosts << "8ad512e339e44954853a9c0f43739dc7.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
